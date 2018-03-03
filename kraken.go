4f69c1eca9167c9bc7a516854223624ed97c403d
Alexandre Jacquin <kinou@rilynett.com>
2018-03-03 17:58:57 +0100

    add kraken class

---
+++ kraken.go
@@ -0,0 +1,36 @@
+package gonavitia
+
+import (
+	"errors"
+	"github.com/golang/protobuf/proto"
+	"github.com/kinnou02/pbnavitia"
+	zmq "github.com/pebbe/zmq2"
+	"time"
+)
+
+type Kraken struct {
+	Name    string
+	Addr    string
+	Timeout time.Duration
+}
+
+func (k *Kraken) Call(request *pbnavitia.Request) (*pbnavitia.Response, error) {
+	requester, _ := zmq.NewSocket(zmq.REQ)
+	requester.Connect(k.Addr)
+	defer requester.Close()
+	data, _ := proto.Marshal(request)
+	requester.Send(string(data), 0)
+	poller := zmq.NewPoller()
+	poller.Add(requester, zmq.POLLIN)
+	p, err := poller.Poll(k.Timeout)
+	if err != nil {
+		return nil, err
+	}
+	if len(p) < 1 {
+		return nil, errors.New("fucked")
+	}
+	raw_resp, _ := p[0].Socket.Recv(0)
+	resp := &pbnavitia.Response{}
+	_ = proto.Unmarshal([]byte(raw_resp), resp)
+	return resp, nil
+}
