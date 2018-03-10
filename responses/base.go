455704234e07717adc83f686840fdbeeac775d9b
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2018-03-10 18:59:53 +0100

    Add StopAreas, StopPoints, Addresses, Admins

--- responses/base.go
+++ responses/base.go
@@ -6,10 +6,14 @@ import (
 )
 
 type Place struct {
-	Id           *string `json:"id"`
-	Name         *string `json:"name"`
-	EmbeddedType *string `json:"embedded_type"`
-	Quality      *int32  `json:"quality,omitempty"`
+	Id           *string    `json:"id"`
+	Name         *string    `json:"name"`
+	EmbeddedType *string    `json:"embedded_type"`
+	Quality      *int32     `json:"quality,omitempty"`
+	StopPoint    *StopPoint `json:"stop_point,omitempty"`
+	StopArea     *StopArea  `json:"stop_area,omitempty"`
+	Admin        *Admin     `json:"administrative_region,omitempty"`
+	Address      *Address   `json:"address,omitempty"`
 }
 
 type NavitiaDatetime time.Time
@@ -18,4 +22,44 @@ func (t NavitiaDatetime) MarshalJSON() ([]byte, error) {
 	//do your serializing here
 	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("20060102T150405"))
 	return []byte(stamp), nil
+}
+
+type Coord struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type StopArea struct {
+	Id       *string  `json:"id"`
+	Name     *string  `json:"name"`
+	Label    *string  `json:"label"`
+	Timezone *string  `json:"Timezone,omitempty"`
+	Coord    *Coord   `json:"coord"`
+	Admins   []*Admin `json:"administrative_regions"`
+}
+
+type StopPoint struct {
+	Id     *string  `json:"id"`
+	Name   *string  `json:"name"`
+	Label  *string  `json:"label"`
+	Coord  *Coord   `json:"coord"`
+	Admins []*Admin `json:"administrative_regions"`
+}
+
+type Admin struct {
+	Id      *string `json:"id"`
+	Name    *string `json:"name"`
+	Label   *string `json:"label"`
+	Coord   *Coord  `json:"coord"`
+	Insee   *string `json:"insee,omitempty"`
+	ZipCode *string `json:"zip_code,omitempty"`
+}
+
+type Address struct {
+	Id          *string  `json:"id"`
+	Name        *string  `json:"name"`
+	Label       *string  `json:"label"`
+	Coord       *Coord   `json:"coord"`
+	HouseNumber *int32   `json:"house_number,omitempty"`
+	Admins      []*Admin `json:"administrative_regions"`
 }
--- serializer/base.go
+++ serializer/base.go
@@ -15,6 +15,89 @@ func NewPlace(pb *pbnavitia.PtObject) *responses.Place {
 		Name:         pb.Name,
 		EmbeddedType: &t,
 		Quality:      pb.Quality,
+		StopPoint:    NewStopPoint(pb.StopPoint),
+		StopArea:     NewStopArea(pb.StopArea),
+		Admin:        NewAdmin(pb.AdministrativeRegion),
+		Address:      NewAddress(pb.Address),
 	}
 	return &place
+}
+
+func NewAdmin(pb *pbnavitia.AdministrativeRegion) *responses.Admin {
+	if pb == nil {
+		return nil
+	}
+	admin := responses.Admin{
+		Id:      pb.Uri,
+		Name:    pb.Name,
+		Label:   pb.Label,
+		Coord:   NewCoord(pb.Coord),
+		Insee:   pb.Insee,
+		ZipCode: pb.ZipCode,
+	}
+	return &admin
+}
+
+func NewCoord(pb *pbnavitia.GeographicalCoord) *responses.Coord {
+	if pb == nil {
+		return nil
+	}
+	coord := responses.Coord{
+		Lat: pb.GetLat(),
+		Lon: pb.GetLon(),
+	}
+	return &coord
+}
+
+func NewStopPoint(pb *pbnavitia.StopPoint) *responses.StopPoint {
+	if pb == nil {
+		return nil
+	}
+	sp := responses.StopPoint{
+		Id:     pb.Uri,
+		Name:   pb.Name,
+		Label:  pb.Label,
+		Coord:  NewCoord(pb.Coord),
+		Admins: make([]*responses.Admin, 0),
+	}
+	for _, pb_admin := range pb.AdministrativeRegions {
+		sp.Admins = append(sp.Admins, NewAdmin(pb_admin))
+	}
+	return &sp
+}
+
+func NewStopArea(pb *pbnavitia.StopArea) *responses.StopArea {
+	if pb == nil {
+		return nil
+	}
+	sa := responses.StopArea{
+		Id:       pb.Uri,
+		Name:     pb.Name,
+		Label:    pb.Label,
+		Timezone: pb.Timezone,
+		Coord:    NewCoord(pb.Coord),
+		Admins:   make([]*responses.Admin, 0),
+	}
+	for _, pb_admin := range pb.AdministrativeRegions {
+		sa.Admins = append(sa.Admins, NewAdmin(pb_admin))
+	}
+	return &sa
+}
+
+func NewAddress(pb *pbnavitia.Address) *responses.Address {
+	if pb == nil {
+		return nil
+	}
+	address := responses.Address{
+		Id:          pb.Uri,
+		Name:        pb.Name,
+		Label:       pb.Label,
+		Coord:       NewCoord(pb.Coord),
+		HouseNumber: pb.HouseNumber,
+		Admins:      make([]*responses.Admin, 0),
+	}
+	for _, pb_admin := range pb.AdministrativeRegions {
+		address.Admins = append(address.Admins, NewAdmin(pb_admin))
+	}
+	return &address
 }
